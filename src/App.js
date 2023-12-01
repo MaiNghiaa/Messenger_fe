2fb0e2ecf7b328ec3fa861a399f9b96fe6a06439
Trung Nghĩa <109063567+MaiNghiaa@users.noreply.github.com>
2023-12-01 16:03:56 +0700

    cautruc

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./index.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-[10px] font-bold underline ">Hello world!</h1>
+      <h1 className="text-[10px] font-bold underline text-sky-500 ">
+        Hello world!
+      </h1>
     </div>
   );
 }
